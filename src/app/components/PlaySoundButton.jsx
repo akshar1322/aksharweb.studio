ec1bb5f3378a2615b388fc7044affca298e53a17
AksharPatel <aksharpatel528@gmail.com>
2024-05-26 11:01:07 +0530

    chore: Update PlaySoundButton component to use next/image for better performance

--- src/app/components/PlaySoundButton.jsx
+++ src/app/components/PlaySoundButton.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-
-
-
+import Image from 'next/image';
 
 const PlaySoundButton = () => {
   // Function to play sound
-  const playSound = () => {
+  const playSound = async () => {
     const audio = new Audio('/Audio/Akshar.mp3');
-    audio.play();
+    await audio.play();
   };
 
   return (
-    <button id="soundBtn" className="bg-transparent p-0 top-1 right-10 w-6 h-6 " onClick={playSound}>
-      <img src="/images/comment-regular.png" alt="sound" />
-
+    <button id="soundBtn" className="bg-transparent p-0 top-1 right-10 w-6 h-6" onClick={playSound}>
+      <Image src="/images/comment-regular.png" alt="sound" width={24} height={24} />
     </button>
   );
 };
