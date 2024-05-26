bc611bb3245e2397e346f4a55682e7a873285ecc
AksharPatel <aksharpatel528@gmail.com>
2024-05-26 10:57:58 +0530

    chore: Update AboutSection component formatting and content

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -39,20 +39,20 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-         <li>AutoCAD Proficiency (Sopan Institute) </li>
+         <li>AutoCAD Proficiency (Sopan Institute)</li>
         <li>Web Development Expertise (Way to Web)</li> 
-        <li>Oracle SQL Mastery (IBM India) </li>
+        <li>Oracle SQL Mastery (IBM India)</li>
       </ul>
     ),
   },
   {
-    title:"Experience",
+    title: "Experience",
     id: "experience",
     content: (
-      <ul className='list-disc pl-2' >
-        <li>Freelancing </li>
-        <li>Web Developer at Way To Web </li> 
-        <li>website maintenance & support at IK Controls </li>
+      <ul className="list-disc pl-2">
+        <li>Freelancing</li>
+        <li>Web Developer at Way To Web</li> 
+        <li>Website maintenance & support at IK Controls</li>
       </ul>
     ),
   },
@@ -71,41 +71,42 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image className='rounded-3xl' 
-        src="/images/three_poster.jpg" width={500} height={500} />
+        <Image
+          className="rounded-3xl"
+          src="/images/three_poster.jpg"
+          alt="Poster Image"
+          width={500}
+          height={500}
+        />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I'm a Full Stack Developer and UI/UX Designer crafting exceptional digital experiences. I bring together technical expertise in JavaScript, React, Node.js, and MongoDB with a passion for beautiful and intuitive design. Always eager to learn and grow, I'm excited for new challenges!
+            I&apos;m a Full Stack Developer and UI/UX Designer crafting exceptional digital experiences. I bring together technical expertise in JavaScript, React, Node.js, and MongoDB with a passion for beautiful and intuitive design. Always eager to learn and grow, I&apos;m excited for new challenges!
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
               selectTab={() => handleTabChange("skills")}
               active={tab === "skills"}
             >
-              {" "}
-              Skills{" "}
+              Skills
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("education")}
               active={tab === "education"}
             >
-              {" "}
-              Education{" "}
+              Education
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("certifications")}
               active={tab === "certifications"}
             >
-              {" "}
-              Certifications{" "}
+              Certifications
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("experience")}
               active={tab === "experience"}
             >
-              {" "}
-              Experience{" "}
+              Experience
             </TabButton>
           </div>
           <div className="mt-8">
